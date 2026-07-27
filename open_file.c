90c0e4e15d60cf092dc4265d742bd222418e4269
waltertaya <waltermittybecker5@gmail.com>
2026-07-27 17:43:04 +0300

    identify video stream & decoder

--- open_file.c
+++ open_file.c
@@ -1,4 +1,5 @@
 #include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
@@ -40,7 +41,33 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    printf("Video stream index: %d\n", video_stream_index);
+    // printf("Video stream index: %d\n", video_stream_index);
+    // Find the registered decoder for out specific video stream
+    AVCodec *codec = avcodec_find_decoder(format_context->streams[video_stream_index]->codecpar->codec_id);
+
+    if (!codec) {
+        printf("Error: Unsupported codec\n");
+        return -1;
+    }
+
+    // allocate an empty context our decoder
+    AVCodecContext *codec_context = avcodec_alloc_context3(codec);
+    if (!codec_context) {
+        printf("Error: Could not allocate codec context\n");
+        return -1;
+    }
+
+    // copy our stream's configurations into the decoder context
+    if (avcodec_parameters_to_context(codec_context, format_context->streams[video_stream_index]->codecpar) < 0) {
+        printf("Error: Could not copy codec paramaters\n");
+        return -1;
+    }
+
+    // open the decoder to make it ready for active decoding
+    if (avcodec_open2(codec_context, codec, NULL) < 0) {
+        printf("Error: Could not open codec\n");
+        return -1;
+    }
 
     // clean up by closing the input context
     avformat_close_input(&format_context);
